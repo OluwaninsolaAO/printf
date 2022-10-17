1d362072076ae407e93927313f7ca34cfd02bd95
Nathanaelfowler <natafowler1@gmail.com>
2022-10-17 09:55:06 -0700

    included files

--- main.h
+++ main.h
@@ -1,4 +1,19 @@
 #ifndef MAIN_H
-#define MAIN_h
+#define MAIN_H
 
+#include <stdarg.h>
 
+/**
+ * struct print - structure for printing various types
+ * @t: type to print
+ * @f: function to print
+ */
+typedef struct print
+{
+	char *t;
+	int (*f)(va_list);
+} print_t;
+
+int _putchar(char c);
+
+#endif /*MAIN_H*/
